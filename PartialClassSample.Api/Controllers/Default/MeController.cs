f023db4edce071ab8e9e21527bcd9d880daa7382
Leonardo Ferreira <leo.oliveira.eng@outlook.com>
2021-04-21 19:37:50 -0300

    project structure

---
+++ PartialClassSample.Api/Controllers/Default/MeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartialClassSample.Api.Controllers.Default
+{
+    [ApiController, Route("api/[controller]")]
+    public class MeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { name = "Partial Class Sample", version = "1.0.0" });
+    }
+}
