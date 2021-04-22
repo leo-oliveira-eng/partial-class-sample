76d95c952050be1f879336232c7c38943155f327
Leonardo Ferreira <leo.oliveira.eng@outlook.com>
2021-04-22 09:05:08 -0300

    implementing register repository

---
+++ PartialClassSample.Api/Data/RegisterRepository.cs
@@ -0,0 +1,30 @@
+﻿using Messages.Core;
+using Microsoft.EntityFrameworkCore;
+using PartialClassSample.Api.Models;
+using PartialClassSample.Api.Models.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PartialClassSample.Api.Data
+{
+    public class RegisterRepository : IRegisterRepository
+    {
+        protected DbContext Context { get; }
+
+        public DbSet<Register> DbSet => Context.Set<Register>();
+
+        public RegisterRepository(UserContext context) => Context = context;
+
+        public Task<List<Register>> GetAllAsync()
+            => DbSet.ToListAsync();
+
+        public async Task<Maybe<Register>> FindAsync(int id)
+            => await DbSet.SingleOrDefaultAsync(register => register.Id == id);
+
+        public Task AddAsync(Register register)
+        {
+            DbSet.Add(register);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ PartialClassSample.Api/Models/Repositories/IRegisterRepository.cs
@@ -0,0 +1,15 @@
+﻿using Messages.Core;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PartialClassSample.Api.Models.Repositories
+{
+    public interface IRegisterRepository
+    {
+        Task<List<Register>> GetAllAsync();
+
+        Task<Maybe<Register>> FindAsync(int id);
+
+        Task AddAsync(Register register);
+    }
+}
